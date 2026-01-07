6bb0c93e958ab3f69bfbc36e50860677e97b9081
Ngozi <hexwhyzed01@gmail.com>
2026-01-07 02:27:47 +0100

    Create user model

---
+++ models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose.Schema;
+
+const userModel = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    status: {
+      type: String,
+      enum: ["active", "suspended"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userModel);
