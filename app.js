d97279f732a88dc2109a909423503987bc200c1a
Ngozi <hexwhyzed01@gmail.com>
2026-01-21 15:01:39 +0100

    Add the middleware to handle joi validatigon error

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 const express = require("express");
 require("dotenv").config();
 
-const db = require("./database/main")
+const db = require("./database/main");
 
 let port = process.env.PORT || 2000;
 
 const app = express();
 
-const authRoute = require('./routes/auth.route')
+const authRoute = require("./routes/auth.route");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", authRoute);
 app.use("/", (req, res, next) => {
-  res.send("IAM Microservice")
-})
+  res.send("IAM Microservice");
+});
+
+//Middleware to handle Joi validation error
+app.use(((err, req, res, next) => {
+  if (err && err.error && err.error.isJoi) {
+    // we had a joi error, let's return a custom 400 json response
+    res.status(400).json({
+      type: err.type, // will be "query" here, but could be "headers", "body", or "params"
+      message: err.error.toString()
+    });
+  } else {
+    // pass on to another error handler
+    next(err);
+     }
+}))
 
 //Middleware to handle the error
-app.use((err, req, res, next) => {
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message | "Internal Server Error"
   res.status(statusCode).json({
     success: false,
     "Status Code" : statusCode,
@@ -33,7 +52,6 @@ db.on("disconnected", () => {
   console.log("MBD Disconnected");
 });
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`)
-})
-  
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
