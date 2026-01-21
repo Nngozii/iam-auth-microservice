8226b6f43bba2e037b5148de50569b79651cfb30
Ngozi <hexwhyzed01@gmail.com>
2026-01-21 15:07:46 +0100

    Rewrite the code whole and format

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
-const authSchema = require("../validators/auth.validator");
-const validator = require('express-joi-validation').createValidator({passError: true})
+const authSchema = require("../validators/auth.validator").authSchema;
+const validator = require("express-joi-validation").createValidator({
+  passError: true,
+});
 
-const authController = require("../controllers/auth.controller")
+const authController = require("../controllers/auth.controller");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/signup", validator.body(authSchema), authController.signUp)
+router.post("/signup", validator.body(authSchema), authController.signUp);
 
 module.exports = router;
