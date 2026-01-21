6f702dd39c4f88bfd392ef40f75135898523c0d3
Ngozi <hexwhyzed01@gmail.com>
2026-01-21 15:09:49 +0100

    Add a Joi validator file to handle user input validation before controller runs

--- validators/auth.validator.js
+++ validators/auth.validator.js
@@ -1,6 +1,5 @@
 const Joi = require('joi')
 
-
 exports.authSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).min(8).required(),
