d1420e27995ae70c2e399d92a728d1ad1c5a8dd0
Ngozi <hexwhyzed01@gmail.com>
2026-01-22 08:17:30 +0100

    Add a comment to tell what the funcion does

--- utilities/error.js
+++ utilities/error.js
@@ -1,3 +1,4 @@
+//A function to create/throw custom errors and to be handles by the middleware in app.js
 function errorHandler(statusCode, message) {
     const error = new Error
     error.statusCode = statusCode
