634530f2787143d41df9d7f1fc63b2780e418bfa
Ngozi <hexwhyzed01@gmail.com>
2026-01-22 09:47:55 +0100

    Add a role validation schema

--- validators/auth.validator.js
+++ validators/auth.validator.js
@@ -3,6 +3,7 @@ const Joi = require('joi')
 exports.authSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).min(8).required(),
-  confirmPassword: Joi.ref("password")
+  confirmPassword: Joi.ref("password"),
+  role: Joi.string().valid("user", "admin").required()
 })
 
