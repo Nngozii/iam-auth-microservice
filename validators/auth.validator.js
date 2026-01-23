0de869f2bac4cfe4002d1bf0eb6fd3e61f96dc3c
Ngozi <hexwhyzed01@gmail.com>
2026-01-23 05:29:36 +0100

    feat: Include joi login schema for verifying user log in

--- validators/auth.validator.js
+++ validators/auth.validator.js
@@ -7,3 +7,7 @@ exports.signupSchema = Joi.object({
   role: Joi.string().valid("user", "admin").required()
 })
 
+exports.loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).min(8).required(),
+})
