bbe4268fe5d21417a91c53172f5b3782964d7ced
Ngozi <hexwhyzed01@gmail.com>
2026-01-23 05:16:58 +0100

    feat: Add the log in route for user log in

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const signupSchema = require("../validators/auth.validator").signupSchema;
+const loginSchema = require("../validators/auth.validator").loginSchema;
 const validator = require("express-joi-validation").createValidator({
   passError: true,
 });
@@ -11,6 +12,6 @@ const router = express.Router();
 
 router.post("/signup", validator.body(signupSchema), authController.signUp);
 
-router.post("/login", authController.logIn)
+router.post("/login", validator.body(loginSchema), authController.logIn)
 
 module.exports = router;
